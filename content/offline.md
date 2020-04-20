3801558ef5fd37c33cfc57486d924427bf15eed5
lislis <mail@lislis.de>
2020-04-20 19:19:26 +0200

    add sw and offline fallbakcs

---
+++ content/offline.md
@@ -0,0 +1,5 @@
+---
+title: You are offline
+---
+
+Check your internet connection.
---
+++ themes/feminicide-theme/static/sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'feminizidmap-cache-v1';
+var urlsToCache = [
+  '/',
+  '/de/',
+  '/offline/',
+  '/sass/site.css',
+  '/js/vendor.js',
+  '/js/main.js',
+  '/fonts/StabilGrotesk-Regular.woff',
+  '/fonts/Blanco-Regular.woff2',
+  '/bg-gradient-01.webp',
+  '/bg-gradient-02.webp'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fromCache(event.request))
+
+  event.waitUntil(update(event.request)
+                  .then(refresh))
+});
+
+
+const fromCache = request => {
+  return caches.open(CACHE_NAME)
+    .then(cache => cache.match(request))
+}
+
+const update = request => {
+  return caches.open(CACHE_NAME)
+    .then(cache => fetch(request)
+          .then(response => cache.put(request, response.clone())
+                .then(() => response))
+          .catch(error => {
+            console.log('[onfetch] Failed, serving fallback ' + error)
+            return caches.open(CACHE_NAME).then(cache => cache.match('/offline/'))
+          }))
+}
+
+const refresh = response => {
+  return self.clients.matchAll()
+    .then(clients => clients.forEach(client => {
+      let message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag')
+      }
+
+      client.postMessage(JSON.stringify(message))
+    }))
+}
