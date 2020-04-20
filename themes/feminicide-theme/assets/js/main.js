4bc39d406bbd98cb5cd55209627d4c58a892f659
lislis <mail@lislis.de>
2020-04-20 15:51:42 +0200

    ignore public folder, move static assets to theme

--- themes/feminicide-theme/assets/js/main.js
+++ themes/feminicide-theme/assets/js/main.js
@@ -1,4 +1,22 @@
-console.log('foo');
+async function supportsWebp() {
+  if (!self.createImageBitmap) return false;
+
+  const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+  const blob = await fetch(webpData).then(r => r.blob());
+  return createImageBitmap(blob).then(() => true, () => false);
+}
+
+(async () => {
+  if(await supportsWebp()) {
+    document.querySelector('body').classList.add('has-webp');
+  } /*
+  else {
+    console.log('does not support');
+  }
+*/
+})();
+
+
 
 barba.use(barbaCss);
 // basic default transition (with no rules and minimal hooks)
